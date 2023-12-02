0a691fe752cdc6b7526f6a8d38a96ad5ae86acdf
mabuelsaad657 <michaelabuelsaad101@gmail.com>
2023-12-02 02:23:41 -0800

    Initial Commit

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Michael Abuelsaad on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
